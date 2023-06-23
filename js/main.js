7538d0c14f10c0f21cb0045648565193fa553213
Alexander <pham0142@algonquinlive.com>
2023-06-23 09:25:54 -0400

    updates js structure

--- js/main.js
+++ js/main.js
@@ -5,9 +5,23 @@ const employee = {
     hireMonth: "Aug",
     hireYear: 2001,
     email:"jaxcompany",
-    department:"Innovation"
+    department:"Innovation",
+    skills: ["UX", "prototyping", "research","documentation"]
 }
 
 const empSection = document.getElementById("employee")
 
 empSection.insertAdjacentHTML("afterbegin", `<h2>Welcome ${employee.name}!</h2>`)
+
+const empInfo = `
+<dl>
+    <dt>ID</dt>
+    <dd>${employee.id}</dd>
+    <dt>Department</dt>
+    <dd>${employee.department}</dd>
+    <dt>Year Hired</dt>
+    <dd>${employee.hireYear}</dd>
+    <dt>Email</dt>
+    <dd>${employee.email}</dd>
+</dl>
+`
